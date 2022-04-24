2b2c9bce079ce125ba2265d17517c9e4770527e6
Victor Chicu <victor@aroundwire.com>
2022-04-24 21:52:59 +0300

    Added test to list users and assert fot http status and response objects count.

--- src/test/java/com/cubicfox/TestBase.java
+++ src/test/java/com/cubicfox/TestBase.java
@@ -1,6 +1,5 @@
 package com.cubicfox;
 
-import com.cubicfox.dto.UserDto;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,35 +8,30 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 import java.io.IOException;
 import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @TestPropertySource("classpath:application.properties")
 @AutoConfigureMockMvc
 public class TestBase {
+    private static final String API_USERS = "/api/users";
+
     @Autowired
     protected MockMvc mockMvc;
     @Autowired
     protected ObjectMapper objectMapper;
 
-    protected List<UserDto> fetchUsers() throws Exception {
-        String jsonString = this.mockMvc.perform(get("/api/users")
-                .accept(MediaType.APPLICATION_JSON_VALUE))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andReturn()
-                .getResponse()
-                .getContentAsString();
-        return toList(jsonString, new TypeReference<List<UserDto>>() {});
+    protected ResultActions fetchUsers() throws Exception {
+        return this.mockMvc.perform(get(API_USERS).accept(MediaType.APPLICATION_JSON_VALUE)).andDo(print());
     }
 
-    private <T> List<T> toList(String jsonString, TypeReference<List<T>> typeReference) throws IOException {
+    protected <T> List<T> toList(String jsonString, TypeReference<List<T>> typeReference) throws IOException {
         return objectMapper.readValue(jsonString, typeReference);
     }
 }
--- src/test/java/com/cubicfox/controllers/UserControllerIT.java
+++ src/test/java/com/cubicfox/controllers/UserControllerIT.java
@@ -2,15 +2,24 @@ package com.cubicfox.controllers;
 
 import com.cubicfox.TestBase;
 import com.cubicfox.dto.UserDto;
+import com.fasterxml.jackson.core.type.TypeReference;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.util.Assert;
 
 import java.util.List;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 public class UserControllerIT extends TestBase {
     @Test
-    public void should_list_users() throws Exception {
-        List<UserDto> actualUsers = fetchUsers();
-        Assert.notEmpty(actualUsers, "Users are empty");
+    public void should_list_users_then_expect_200_http_status() throws Exception {
+        for (int i = 0; i < 5; i++) {
+            List<UserDto> actualUsers = toList(
+                    fetchUsers().andExpect(status().isOk()).andReturn().getResponse().getContentAsString(),
+                    new TypeReference<List<UserDto>>() {}
+            );
+            Assertions.assertFalse(actualUsers.isEmpty(), "actualUsers should not be empty");
+            Assertions.assertEquals(10, actualUsers.size());
+        }
     }
 }
